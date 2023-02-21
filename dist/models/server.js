98eaf732a0a005d7d8a6f9c8205b3d2557e9d610
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-02-21 13:12:46 -0300

    Middlewares config (Cors, Body parser and Public Path)

--- dist/models/server.js
+++ dist/models/server.js
@@ -4,10 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const usuarios_1 = __importDefault(require("../routes/usuarios"));
+const cors_1 = __importDefault(require("cors"));
 class Server {
     constructor() {
+        this.apiPaths = {
+            usuarios: '/api/usuarios',
+        };
         this.app = (0, express_1.default)();
         this.port = process.env.PORT || '8000';
+        //definir rutas//
+        this.middlewares();
+        this.routes();
+    }
+    //TODO: conectar base de datos
+    middlewares() {
+        this.app.use((0, cors_1.default)());
+        //lectura del body
+        this.app.use(express_1.default.json());
+        //carpeta publica
+        this.app.use(express_1.default.static('public'));
+    }
+    routes() {
+        this.app.use(this.apiPaths.usuarios, usuarios_1.default);
     }
     listen() {
         this.app.listen(this.port, () => {
--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,38 @@
 import express, {Application} from 'express';
+import userRoutes from '../routes/usuarios';
+import cors from 'cors';
+
 
 class Server{
 
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios',
+    }
+
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8000';
+        //definir rutas//
+        this.middlewares();
+        this.routes();
+    }
+
+    //TODO: conectar base de datos
+
+    middlewares(){
+        this.app.use(cors())
+
+        //lectura del body
+        this.app.use(express.json())
+
+        //carpeta publica
+        this.app.use(express.static('public'))
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRoutes)
     }
 
     listen(){
